8fe8cd4de73a6fa4022fd90a9477d9500a683ff8
Alex Suraci <suraci.alex@gmail.com>
2023-09-14 00:30:41 -0400

    docs for LatestSemverTag

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ type Gitutil struct{}
 
 // DefaultBranch returns the default branch of a git repository.
 func (m *Gitutil) DefaultBranch(ctx context.Context, gitBase *Container, repo string) (string, error) {
+	// TODO: this should not be cached. in practice it doesn't change often, but
+	// someone setting up a repo for the first time (i.e. a freshly baked Module)
+	// might change it soon after indexing.
+
 	output, err := gitBase.
 		WithExec([]string{"git", "ls-remote", "--symref", repo, "HEAD"}, ContainerWithExecOpts{
 			SkipEntrypoint: true,
@@ -39,7 +43,14 @@ func (m *Gitutil) DefaultBranch(ctx context.Context, gitBase *Container, repo st
 	return "", fmt.Errorf("could not deduce default branch from output:\n%s", output)
 }
 
-func (m *Gitutil) LatestSemverTag(ctx context.Context, gitBase *Container, repo string, prefix string) (string, error) {
+// LatestSemverTag returns the latest semver tag of a git repository.
+//
+// It accepts an optional prefix which can be used to filter tags. For example,
+// if you have multiple sub-directories in a monorepo you might have a
+// convention like sub/path/v1.2.3.
+func (m *Gitutil) LatestSemverTag(ctx context.Context, gitBase *Container, repo, prefix string) (string, error) {
+	// TODO: this really should not be cached
+
 	output, err := gitBase.
 		WithExec([]string{"git", "ls-remote", "--tags", repo, prefix + "v*"}, ContainerWithExecOpts{
 			SkipEntrypoint: true,
